74c8786fae49a9779cfb243070129b332f3c64db
Luiz Junior <luizs.araujo@sicoob.com.br>
2022-11-25 00:03:30 -0300

    Aula sobre operator methods

---
+++ projetos_aula/dart_poo/lib/13_Operator_methods/1_operator_methods.dart
@@ -0,0 +1,9 @@
+import 'package:dart_poo/13_Operator_methods/numero.dart';
+
+void main(List<String> args) {
+  var num1 = Numero(10);
+  var num2 = Numero(60);
+
+  var totalConta = num1 - num2;
+  print(totalConta.i);
+}
